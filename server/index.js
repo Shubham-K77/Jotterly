16920cec4c4b8cf000aa163848f0d9ad9f0c686d
Shubham Kadariya <shubham.kadariya021@apexcollege.edu.np>
2025-04-04 12:48:01 +0545

    Token Fix!

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ app.use(
   cors({
     origin: "https://jotterly.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["set-cookie"],
   })
 );
 app.use(cookieParser());
